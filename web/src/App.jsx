620d6ce8130d21aad16fbaebff921db10c24eeb6
Yuriferr <yurifernandespreto@gmail.com>
2025-01-22 10:15:40 -0300

    ajustes no front-end

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react'
-import './App.scss'
+import { useEffect, useState } from 'react';
+import './App.scss';
 
-import axios from 'axios'
+import axios from 'axios';
 
 function App() {
-  const [pokemon, setPokemon] = useState("bulbasaur")
+  const [pokemon, setPokemon] = useState("bulbasaur");
   const [dados, setDados] = useState({
     name: '',
     id: '',
@@ -12,7 +12,9 @@ function App() {
     types: [],
     stats: [],
     abilities: [],
-  })
+  });
+  
+  const [pokemonList, setPokemonList] = useState([]);
   const colorsTypes = {
     'bug': 'DarkGreen',
     'dark': 'grey',
@@ -32,115 +34,99 @@ function App() {
     'rock': 'Maroon',
     'steel': 'MediumAquamarine',
     'water': 'blue'
-  }
+  };
 
   useEffect(() => {
-    getDados()
-    typesColor()
-  }, [pokemon, dados])
+    getDados();
+  }, [pokemon]);
 
-  async function getDados(){
-    await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`).then(function (response){
-      setDados({
-          name: response.data.name,
-          id: response.data.id,
-          img: response.data.sprites.front_default,
-          types: response.data.types,
-          stats: response.data.stats,
-          abilities: response.data.abilities,
-        }
-      )
-    })
+  useEffect(() => {
+    async function fetchPokemonList() {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
+      setPokemonList(response.data.results.map(p => p.name));
+    }
+    fetchPokemonList();
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'ArrowLeft') {
+        changePokemon(-1);
+      } else if (event.key === 'ArrowRight') {
+        changePokemon(1);
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [pokemon, pokemonList]);
+
+  async function getDados() {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    setDados({
+      name: response.data.name,
+      id: response.data.id,
+      img: response.data.sprites.front_default,
+      types: response.data.types,
+      stats: response.data.stats,
+      abilities: response.data.abilities,
+    });
+    typesColor(response.data.types);
   }
 
-  function typesColor(){
-    var backgroud = document.getElementById('container-App')
+  function changePokemon(direction) {
+    const currentIndex = pokemonList.indexOf(pokemon.toLowerCase());
+    if (currentIndex !== -1) {
+      const nextIndex = (currentIndex + direction + pokemonList.length) % pokemonList.length;
+      setPokemon(pokemonList[nextIndex]);
+    }
+  }
 
-    if(dados.types[0]?.type.name == 'bug'){
-      backgroud.style.backgroundColor = 'DarkGreen'
-    }else if(dados.types[0]?.type.name == 'dark'){
-      backgroud.style.backgroundColor = 'grey'
-    }else if(dados.types[0]?.type.name == 'dragon'){
-      backgroud.style.backgroundColor = 'DeepSkyBlue'
-    }else if(dados.types[0]?.type.name == 'electric'){
-      backgroud.style.backgroundColor = 'Yellow'
-    }else if(dados.types[0]?.type.name == 'fairy'){
-      backgroud.style.backgroundColor = 'MediumVioletRed'
-    }else if(dados.types[0]?.type.name == 'fighting'){
-      backgroud.style.backgroundColor = 'Orange'
-    }else if(dados.types[0]?.type.name == 'fire'){
-      backgroud.style.backgroundColor = 'red'
-    }else if(dados.types[0]?.type.name == 'flying'){
-      backgroud.style.backgroundColor = 'CadetBlue'
-    }else if(dados.types[0]?.type.name == 'ghost'){
-      backgroud.style.backgroundColor = 'Indigo'
-    }else if(dados.types[0]?.type.name == 'grass'){
-      backgroud.style.backgroundColor = 'green'
-    }else if(dados.types[0]?.type.name == 'ground'){
-      backgroud.style.backgroundColor = 'Chocolate'
-    }else if(dados.types[0]?.type.name == 'ice'){
-      backgroud.style.backgroundColor = 'Aqua'
-    }else if(dados.types[0]?.type.name == 'normal'){
-      backgroud.style.backgroundColor = 'PaleVioletRed'
-    }else if(dados.types[0]?.type.name == 'poison'){
-      backgroud.style.backgroundColor = 'Purple'
-    }else if(dados.types[0]?.type.name == 'psychic'){
-      backgroud.style.backgroundColor = 'DeepPink'
-    }else if(dados.types[0]?.type.name == 'rock'){
-      backgroud.style.backgroundColor = 'Maroon'
-    }else if(dados.types[0]?.type.name == 'steel'){
-      backgroud.style.backgroundColor = 'MediumAquamarine'
-    }else if(dados.types[0]?.type.name == 'water'){
-      backgroud.style.backgroundColor = 'blue'
+  function typesColor(types) {
+    var backgroud = document.getElementById('container-App');
+    if (backgroud) {
+      const type = types[0]?.type.name;
+      backgroud.style.backgroundColor = colorsTypes[type] || 'white';
     }
   }
 
   return (
     <div id='container-App' className='container-App'>
       <div className='container-Pokedex'>
-
         <div className='container-pesquisa'>
-          <input defaultValue={pokemon} placeholder={pokemon} onChange={(e) => setPokemon(e.target.value)}/>
+          <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
           <p>#{dados.id}</p>
         </div>
-
-        <img src={dados.img}/>
+        <img src={dados.img} alt={dados.name} />
         <p className='name'>{dados.name}</p>
-
         <div className='container-Types'>
-            {dados.types.map((item, index) => {
-                return(
-                    <p key={index} style={{backgroundColor: `${colorsTypes[item.type.name]}`}} className='typeName' value={item.type.name}>{item.type.name}</p>
-                )
-              })}
-          </div>
-
-        <div className='container-Info'>
-              <div className='container-Stats'>
-                <p className='titulo'>Status</p>
-                {dados.stats.map((item, index) => {
-                  return(
-                    <div className='stats' key={index}>
-                      <p>{item.stat.name}:</p>
-                      <p>{item.base_stat}</p>
-                    </div>
-                  )
-                })}
-              </div>
-
-              <div className='container-Abilities'>
-                <p className='titulo'>Habilidades</p>
-                {dados.abilities.map((item, index) => {
-                  return(
-                    <p key={index}>{index + 1}. {item.ability.name}</p>
-                  )
-                })}
-              </div>
+          {dados.types.map((item, index) => (
+            <p key={index} style={{ backgroundColor: colorsTypes[item.type.name] }} className='typeName' value={item.type.name}>
+              {item.type.name}
+            </p>
+          ))}
         </div>
-
+        <div className='container-Info'>
+          <div className='container-Stats'>
+            <p className='titulo'>Status</p>
+            {dados.stats.map((item, index) => (
+              <div className='stats' key={index}>
+                <p>{item.stat.name}:</p>
+                <p>{item.base_stat}</p>
+              </div>
+            ))}
+          </div>
+          <div className='container-Abilities'>
+            <p className='titulo'>Habilidades</p>
+            {dados.abilities.map((item, index) => (
+              <p key={index}>{index + 1}. {item.ability.name}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
